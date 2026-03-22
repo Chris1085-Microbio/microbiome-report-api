b5d833612795118dc802a39bee0928a88c832398
Chris Chiang <itscfchiang@gmail.com>
2026-03-22 20:41:04 +0800

    refactor: Update index.html and report.html for improved content structure and styling. Remove unused font links, replace images, and adjust text formatting. Add new font files and enhance CSS for better readability and design consistency.

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,9 @@
 "use strict";
 
-$(document).ready(function () {// console.log('HesSchool Hello!!')
+$(document).ready(function () {
+  // console.log('HesSchool Hello!!')
+  for (var index = 0; index < array.length; index++) {
+    var element = array[index];
+  }
 });
 //# sourceMappingURL=all.js.map
