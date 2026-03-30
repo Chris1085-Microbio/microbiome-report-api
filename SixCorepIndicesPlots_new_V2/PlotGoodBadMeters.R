1f626444002f77dde0a70e7ebad1ce30487106d8
Chris Chiang <itscfchiang@gmail.com>
2026-03-30 22:03:39 +0800

    refactor: Remove outdated Rplots.pdf and add new visualizations in SixCorepIndicesPlots_new_V2. Introduced new plots and scripts for improved data representation, including distribution and merge plots, along with necessary font files and icons for enhanced styling.

---
+++ SixCorepIndicesPlots_new_V2/PlotGoodBadMeters.R
@@ -0,0 +1,95 @@
+# usage: /pkg/miniconda3/envs/qiime2-2021.11/bin/Rscript PlotGoodBadMeters.R 100 50 0 30 60 70
+# MicrobiomeIndex GoodBadIndex SugerIndex OilIndex ImmuneIndex GutIndex
+library(ggplot2)
+library(magick)
+library(ggforce)
+library(png)
+library(grid)
+library(showtext)
+
+showtext_auto()
+args <- commandArgs(trailingOnly=TRUE)
+if (.Platform$OS.type == "unix") {
+  # Linux / Mac
+  message("Linux/Mac")
+  setwd("/opt/lampp/htdocs/microbiome-report-apiV2_260323/SixCorepIndicesPlots_new_V2")
+  SourcePath = "/opt/lampp/htdocs/microbiome-report-apiV2_260323/SixCorepIndicesPlots_new_V2"
+} else {
+  # Windows
+  message("Windows")
+  library(this.path)
+  setwd(dirname(this.path()))
+  SourcePath = "."
+  args <- c(50, 0, 100, 30, 40, 70)
+}
+
+font_add("ms", regular = "./msjh.ttc", bold = "./msjhbd.ttc")
+MainImg_png_GoodBad <- readPNG(paste0(SourcePath,"/MeterSource/GoodBadMeter.png"))
+MainImg_size_GoodBad = 5
+MainImg_GoodBad <- rasterGrob(MainImg_png_GoodBad, interpolate=TRUE, width  = unit(MainImg_size_GoodBad*2020/1000, "cm"), height = unit(MainImg_size_GoodBad*856/1000, "cm"))
+MainImg_png_HighLow <- readPNG(paste0(SourcePath,"/MeterSource/HighLowMeter.png"))
+MainImg_size_HighLow = 5
+MainImg_HighLow <- rasterGrob(MainImg_png_HighLow, interpolate=TRUE, width  = unit(MainImg_size_HighLow*2020/1000, "cm"), height = unit(MainImg_size_HighLow*856/1000, "cm"))
+scores <- as.numeric(args)
+IndexNames <- c("MicrobiomeIndex", "GoodBadIndex", "SugerIndex", "OilIndex", "ImmuneIndex", "GutIndex")
+Arrow_size <- 0.04
+Arrow_Distance = 0.63
+if (!dir.exists(paste0(SourcePath,"/GoodBadMetersResult"))){dir.create(paste0(SourcePath,"/GoodBadMetersResult"))}
+PlotMeter <- function(x){
+  if (x %in% c(1,3,4,5,6)){
+    MainImg<-MainImg_GoodBad
+  }else{
+    MainImg<-MainImg_HighLow
+  }
+  ArrowAngle <- (scores[x]-100)*(1.8)*-1
+  Arrowradius <- ArrowAngle*pi/180 #Angle to radius = x*pi/180
+  plot.x=c()
+  plot.xTri=c()
+  plot.y=c()
+  plot.yTri=c()
+  plot.color=c('red')
+  Xpos = (cos(Arrowradius))/pi*Arrow_Distance*2
+  Ypos = ((sin(Arrowradius))/pi*Arrow_Distance*2.1)+0.244
+  plot.xTri <- c(
+    Xpos + cos((ArrowAngle+0)  *pi/180) * Arrow_size,
+    Xpos + cos((ArrowAngle+120)*pi/180) * Arrow_size/2,
+    Xpos + cos((ArrowAngle+240)*pi/180) * Arrow_size/2
+  )
+  plot.yTri <- c(
+    Ypos + sin((ArrowAngle+0)  *pi/180) * Arrow_size,
+    Ypos + sin((ArrowAngle+120)*pi/180) * Arrow_size/2,
+    Ypos + sin((ArrowAngle+240)*pi/180) * Arrow_size/2
+  )
+  df<-data.frame(xTri=plot.xTri, yTri=plot.yTri)
+
+  OutFile<-(paste0(SourcePath,"/GoodBadMetersResult","/",IndexNames[x],".png"))
+  p<-ggplot(df, aes(x = xTri, y = yTri)) + 
+    annotation_custom(MainImg, xmin=-Inf, xmax=Inf, ymin=-Inf, ymax=Inf) +
+    geom_shape( expand = unit(0, 'mm'), radius = unit(0, 'mm'), fill = plot.color, color = 'NA',size=0.04) +
+    xlim(-1+0.01,1+0.01)+ylim(0,1) +
+    coord_fixed(ratio = 856*1.5/2020*1.5) + 
+    theme(axis.title=element_blank(),
+          axis.text=element_blank(),
+          axis.ticks=element_blank(),
+          panel.grid = element_blank(),
+          text = element_text(family = "BL"),
+          panel.background = element_rect(fill = "transparent"),
+          plot.background = element_rect(fill = "transparent", color = NA),
+          legend.background = element_rect(fill = "transparent"), # get rid of legend bg
+          legend.box.background = element_rect(fill = "transparent"), # get rid of legend panel bg
+          plot.margin = margin(0, 0, 0, 0)
+    ) 
+  p
+  ggsave(
+    filename = OutFile,
+    plot = p,
+    device = NULL,
+    scale = 1,
+    width = 5,
+    height = 2,
+    limitsize = FALSE,
+    bg = NULL
+  )
+  ArrowAngle
+}
+lapply(seq_along(scores), PlotMeter)
---
+++ SixCorepIndicesPlots_new_V2/PlotRiskMeters.R
@@ -0,0 +1,71 @@
+# usage: /pkg/miniconda3/envs/qiime2-2021.11/bin/Rscript PlotRiskMeters.R 80 60 20 30 40 70 20 30 40 70
+# BrainScore HeartScore LungScore LiverScore KidneyScore StomachScore IntestineScore ImmuneScore ObeseScore MetabolismScore
+library(ggplot2)
+library(magick)
+library(ggforce)
+library(png)
+library(grid)
+library(showtext)
+
+showtext_auto()
+args <- commandArgs(trailingOnly=TRUE)
+if (.Platform$OS.type == "unix") {
+  # Linux / Mac
+  message("Linux/Mac")
+  setwd("/opt/lampp/htdocs/microbiome-report-apiV2_260323/SixCorepIndicesPlots_new_V2")
+  SourcePath = "/opt/lampp/htdocs/microbiome-report-apiV2_260323/SixCorepIndicesPlots_new_V2"
+} else {
+  # Windows
+  message("Windows")
+  library(this.path)
+  setwd(dirname(this.path()))
+  SourcePath = "."
+  args <- c(90, 60, 50, 30, 40, 70, 45, 100, 45, 0)
+}
+
+font_add("ms", regular = "./msjh.ttc", bold = "./msjhbd.ttc")
+MainImg_png <- readPNG(paste0(SourcePath,"/MeterSource/RiskMeter.png"))
+MainImg_size = 10
+MainImg <- rasterGrob(MainImg_png, interpolate=TRUE, width  = unit(MainImg_size/2, "cm"), height = unit(MainImg_size*2, "cm"))
+Arrow_size <- 0.9
+ArrowPng <-  magick::image_read(paste0(SourcePath,"/redTriangle_RiskMeter.png"))
+ArrowAngle <- 270
+ArrowFig <- rasterGrob(magick::image_rotate(ArrowPng, ArrowAngle ), interpolate=TRUE, width  = unit(Arrow_size, "cm"), height = unit(Arrow_size, "cm"))
+scores <- as.numeric(args)/10
+organ_names <- c("BrainScore","HeartScore","LungScore","LiverScore","KidneyScore",
+                 "StomachScore","IntestineScore","ImmuneScore","ObeseScore","MetabolismScore")
+
+if (!dir.exists(paste0(SourcePath,"/RiskMetersResult"))){dir.create(paste0(SourcePath,"/RiskMetersResult"))}
+PlotMeter <- function(x){
+  OutFile<-(paste0(SourcePath,"/RiskMetersResult","/",organ_names[x],".png"))
+  label <- ifelse(scores[x] == 10, "10", sprintf("%.1f", scores[x]))
+  p<-ggplot() + 
+    annotation_custom(MainImg, xmin=-0.25, xmax=Inf, ymin=-Inf, ymax=Inf) +
+    annotation_custom(ArrowFig, xmin=-0.3, xmax=-0.25, ymin=scores[x], ymax=scores[x]) +
+    annotate("text", x = -0.45, y = scores[x], label = label, 
+             hjust = 1, size = 38, fontface="bold") +
+    xlim(-1,1)+ylim(-1.105,11.115) +
+    theme(axis.title=element_blank(),
+          axis.text=element_blank(),
+          axis.ticks=element_blank(),
+          panel.grid = element_blank(),
+          text = element_text(family = "BL"),
+          panel.background = element_rect(fill = "transparent"),
+          plot.background = element_rect(fill = "transparent", color = NA),
+          legend.background = element_rect(fill = "transparent"), # get rid of legend bg
+          legend.box.background = element_rect(fill = "transparent") # get rid of legend panel bg
+    ) 
+  p
+  ggsave(
+    filename = OutFile,
+    plot = p,
+    device = NULL,
+    scale = 1,
+    width = 4,
+    height = 10,
+    limitsize = TRUE,
+    bg = NULL
+  )
+}
+lapply(seq_along(scores), PlotMeter)
+
